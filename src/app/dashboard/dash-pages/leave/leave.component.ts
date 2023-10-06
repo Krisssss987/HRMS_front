f105919d2c90c179880f147dc88d3ccc84adbed0
YatharthDeshpande <yatharthdeshpande724@gmail.com>
2023-10-06 17:55:37 +0530

    table and layout
    made the table and layout for leave and time

--- src/app/dashboard/dash-pages/leave/leave.component.ts
+++ src/app/dashboard/dash-pages/leave/leave.component.ts
@@ -1,26 +1,9 @@
 import { Component, ViewChild } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
-import {MatTableModule} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-leave',
@@ -29,8 +12,45 @@ const ELEMENT_DATA: PeriodicElement[] = [
 
 })
 export class LeaveComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['Employee_ID', 'EmployeeName', 'Role', 'Email', 'PhoneNumber', 'DOB', 'Supervisor', 'ApprovedReject'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+}
+export interface PeriodicElement {
+  Employee_ID: number;
+  EmployeeName: string;
+  Role: string;
+  Email: string;
+  PhoneNumber: string;
+  DOB: string; // Add this property
+  Supervisor: string; // Add this property
+  ApprovedReject: string;
 
 }
   
+const ELEMENT_DATA: PeriodicElement[] = [
+  { Employee_ID: 1, EmployeeName: 'Hydrogen', Role: '1.0079', Email: 'H', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Approved' },
+  { Employee_ID: 2, EmployeeName: 'Helium', Role: '4.0026', Email: 'He', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Rejected' },
+  { Employee_ID: 3, EmployeeName: 'Lithium', Role: '6.941', Email: 'Li', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Approved' },
+  { Employee_ID: 4, EmployeeName: 'Beryllium', Role: '9.0122', Email: 'Be', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Rejected' },
+  { Employee_ID: 5, EmployeeName: 'Boron', Role: '10.811', Email: 'B', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Approved' },
+  { Employee_ID: 6, EmployeeName: 'Carbon', Role: '12.0107', Email: 'C', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Rejected' },
+  { Employee_ID: 7, EmployeeName: 'Nitrogen', Role: '14.0067', Email: 'N', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Approved' },
+  { Employee_ID: 8, EmployeeName: 'Oxygen', Role: '15.9994', Email: 'O', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Rejected' },
+  { Employee_ID: 9, EmployeeName: 'Fluorine', Role: '18.9984', Email: 'F', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Approved' },
+  { Employee_ID: 10, EmployeeName: 'Neon', Role: '20.1797', Email: 'Ne', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Rejected' },
+  { Employee_ID: 11, EmployeeName: 'Sodium', Role: '22.9897', Email: 'Na', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Approved' },
+  { Employee_ID: 12, EmployeeName: 'Magnesium', Role: '24.305', Email: 'Mg', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Rejected' },
+  { Employee_ID: 13, EmployeeName: 'Aluminum', Role: '26.9815', Email: 'Al', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Approved' },
+  { Employee_ID: 14, EmployeeName: 'Silicon', Role: '28.0855', Email: 'Si', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Rejected' },
+  { Employee_ID: 15, EmployeeName: 'Phosphorus', Role: '30.9738', Email: 'P', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Approved' },
+  { Employee_ID: 16, EmployeeName: 'Sulfur', Role: '32.065', Email: 'S', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Rejected' },
+  { Employee_ID: 17, EmployeeName: 'Chlorine', Role: '35.453', Email: 'Cl', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Approved' },
+  { Employee_ID: 18, EmployeeName: 'Argon', Role: '39.948', Email: 'Ar', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Rejected' },
+  { Employee_ID: 19, EmployeeName: 'Potassium', Role: '39.0983', Email: 'K', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Approved' },
+  { Employee_ID: 20, EmployeeName: 'Calcium', Role: '40.078', Email: 'Ca', PhoneNumber: 'H', DOB: 'H', Supervisor: 'H', ApprovedReject: 'Rejected' },
+];
--- src/app/dashboard/dash-pages/time-sheet/time-sheet.component.ts
+++ src/app/dashboard/dash-pages/time-sheet/time-sheet.component.ts
@@ -1,5 +1,10 @@
 import { DatePipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { MatPaginator } from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+
+
 
 
 @Component({
@@ -17,23 +22,42 @@ export class TimeSheetComponent {
     // Prevent Saturday and Sunday from being selected.
     return day !== 0;  
   };
-
-  displayedColumns = [ 'project_title','status','submitted_date','timesheet_date','remark'];
-  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['ProjectTitle', 'Status', 'SubmittedDate', 'TimesheetDate', 'Remarks'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
 }
 
 export interface PeriodicElement {
-  project_title: string;
-  status: string;
-  submitted_date:Date;
-  timesheet_date:Date;
-  remark:string;
+  ProjectTitle: string;
+  Status: string;
+  SubmittedDate: string;
+  TimesheetDate: string;
+  Remarks: string;
+ 
 }
-
 const ELEMENT_DATA: PeriodicElement[] = [
- {project_title:'EMS',status:'Submitted',submitted_date:new Date('2023-10-05'),timesheet_date:new Date('2023-10-05'),remark:'Changes'},
- {project_title:'HRMS',status:'Not Submitted',submitted_date:new Date('2023-10-05'),timesheet_date:new Date('2023-10-05'),remark:'Approved'},
- {project_title:'TMS',status:'Submitted',submitted_date:new Date('2023-10-05'),timesheet_date:new Date('2023-10-05'),remark:'Changes'},
- {project_title:'PP DETECTION',status:'Not Submitted',submitted_date:new Date('2023-10-05'),timesheet_date:new Date('2023-10-05'),remark:'Approved'},
- {project_title:'WMS',status:'Submitted',submitted_date:new Date('2023-10-05'),timesheet_date:new Date('2023-10-05'),remark:'Changes'},
+  { ProjectTitle: '1', Status: 'Hydrogen', SubmittedDate: '1.0079', TimesheetDate: 'H', Remarks: 'H'},
+  { ProjectTitle: '2', Status: 'Helium', SubmittedDate: '4.0026', TimesheetDate: 'He', Remarks: 'H' },
+  { ProjectTitle: '3', Status: 'Lithium', SubmittedDate: '6.941', TimesheetDate: 'Li', Remarks: 'H'},
+  { ProjectTitle: '4', Status: 'Beryllium', SubmittedDate: '9.0122', TimesheetDate: 'Be', Remarks: 'H' },
+  { ProjectTitle: '5', Status: 'Boron', SubmittedDate: '10.811', TimesheetDate: 'B', Remarks: 'H'},
+  { ProjectTitle: '6', Status: 'Carbon', SubmittedDate: '12.0107', TimesheetDate: 'C', Remarks: 'H' },
+  { ProjectTitle: '7', Status: 'Nitrogen', SubmittedDate: '14.0067', TimesheetDate: 'N', Remarks: 'H' },
+  { ProjectTitle: '8', Status: 'Oxygen', SubmittedDate: '15.9994', TimesheetDate: 'O', Remarks: 'H'},
+  { ProjectTitle: '9', Status: 'Fluorine', SubmittedDate: '18.9984', TimesheetDate: 'F', Remarks: 'H' },
+  { ProjectTitle: '10', Status: 'Neon', SubmittedDate: '20.1797', TimesheetDate: 'Ne', Remarks: 'H' },
+  { ProjectTitle: '11', Status: 'Sodium', SubmittedDate: '22.9897', TimesheetDate: 'Na', Remarks: 'H' },
+  { ProjectTitle: '12', Status: 'Magnesium', SubmittedDate: '24.305', TimesheetDate: 'Mg', Remarks: 'H' },
+  { ProjectTitle: '13', Status: 'Aluminum', SubmittedDate: '26.9815', TimesheetDate: 'Al', Remarks: 'H' },
+  { ProjectTitle: '14', Status: 'Silicon', SubmittedDate: '28.0855', TimesheetDate: 'Si', Remarks: 'H' },
+  { ProjectTitle: '15', Status: 'Phosphorus', SubmittedDate: '30.9738', TimesheetDate: 'P', Remarks: 'H'},
+  { ProjectTitle: '16', Status: 'Sulfur', SubmittedDate: '32.065', TimesheetDate: 'S', Remarks: 'H' },
+  { ProjectTitle: '17', Status: 'Chlorine', SubmittedDate: '35.453', TimesheetDate: 'Cl', Remarks: 'H' },
+  { ProjectTitle: '18', Status: 'Argon', SubmittedDate: '39.948', TimesheetDate: 'Ar', Remarks: 'H' },
+  { ProjectTitle: '19', Status: 'Potassium', SubmittedDate: '39.0983', TimesheetDate: 'K', Remarks: 'H' },
+  { ProjectTitle: '20', Status: 'Calcium', SubmittedDate: '40.078', TimesheetDate: 'Ca', Remarks: 'H'},
 ];
+
