6d6ef55190aff970d330a93bf875e83a37744a2e
YatharthDeshpande <yatharthdeshpande724@gmail.com>
2023-10-25 12:41:09 +0530

    localhost

--- src/app/dashboard/dash-pages/employee-management/employee-management.component.ts
+++ src/app/dashboard/dash-pages/employee-management/employee-management.component.ts
@@ -60,9 +60,7 @@ export class EmployeeManagementComponent implements OnInit{
 
  
   displayedColumns: string[] = ['Employee_ID', 'EmployeeName', 'Role', 'Email', 'PhoneNumber', 'DOB', 'Supervisor'];
-
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-
+  dataSource = new MatTableDataSource<PeriodicElement>([]);
   @ViewChild(MatPaginator, {static: true}) paginator!: MatPaginator;
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -96,30 +94,4 @@ export interface PeriodicElement {
   Supervisor: string; // Add this property
 
 }
-
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {Employee_ID: 1, EmployeeName: 'Hydrogen', Role: '1.0079', Email: 'H',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 2, EmployeeName: 'Helium', Role: '4.0026', Email: 'He',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 3, EmployeeName: 'Lithium', Role: '6.941', Email: 'Li',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 4, EmployeeName: 'Beryllium', Role:' 9.0122', Email: 'Be',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 5, EmployeeName: 'Boron', Role:' 10.811', Email: 'B',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 6, EmployeeName: 'Carbon', Role:' 12.0107', Email: 'C',PhoneNumber: 'H',  DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 7, EmployeeName: 'Nitrogen', Role: '14.0067', Email: 'N',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 8, EmployeeName: 'Oxygen', Role: '15.9994', Email: 'O',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 9, EmployeeName: 'Fluorine', Role:' 18.9984', Email: 'F',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 10, EmployeeName: 'Neon', Role: '20.1797', Email: 'Ne',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 11, EmployeeName: 'Sodium', Role: '22.9897', Email: 'Na',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 12, EmployeeName: 'Magnesium', Role: '24.305', Email: 'Mg',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 13, EmployeeName: 'Aluminum', Role: '26.9815', Email: 'Al',PhoneNumber: 'H',  DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 14, EmployeeName: 'Silicon', Role: '28.0855', Email: 'Si',PhoneNumber: 'H',DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 15, EmployeeName: 'Phosphorus', Role: '30.9738', Email: 'P',PhoneNumber: 'H', DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 16, EmployeeName: 'Sulfur', Role: '32.065', Email: 'S',PhoneNumber: 'H',  DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 17, EmployeeName: 'Chlorine', Role: '35.453', Email: 'Cl',PhoneNumber: 'H', DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 18, EmployeeName: 'Argon', Role: '39.948', Email: 'Ar',PhoneNumber: 'H',  DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 19, EmployeeName: 'Potassium', Role: '39.0983', Email: 'K',PhoneNumber: 'H',  DOB: 'H',Supervisor: 'H'},
-  {Employee_ID: 20, EmployeeName: 'Calcium', Role: '40.078', Email: 'Ca',PhoneNumber: 'H',  DOB: 'H',Supervisor: 'H'},
-];
-
-
 
